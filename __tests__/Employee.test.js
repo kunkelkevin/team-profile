c914579fd6627b3c6dbbde63aca2796a6c8f5f0e
Kevin Kunkel <Christopher.Kunkel@traviscountytx.gov>
2021-05-17 08:55:53 -0500

    created Employee and Manager objects

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,3 +7,31 @@ test("Creates an Employee object", () => {
   expect(employee.id).toBe(123);
   expect(employee.email).toBe("john@email.com");
 });
+
+test("getName return the employee name", () => {
+  const employee = new Employee("John Smith", 123, "john@email.com");
+  const name = employee.getName();
+
+  expect(name).toEqual(expect.stringContaining("John Smith"));
+});
+
+test("getId return the a string", () => {
+  const employee = new Employee("John Smith", 123, "john@email.com");
+  const id = employee.getId();
+
+  expect(id).toEqual(expect.stringContaining("123"));
+});
+
+test("getEmail return a string with email", () => {
+  const employee = new Employee("John Smith", 123, "john@email.com");
+  const email = employee.getEmail();
+
+  expect(email).toEqual(expect.stringContaining("john@email.com"));
+});
+
+test("getRole returns Employee", () => {
+  const employee = new Employee("John Smith", 123, "john@email.com");
+  const role = employee.getRole();
+
+  expect(role).toEqual(expect.stringContaining("Employee"));
+});
---
+++ src/page-template.js
@@ -0,0 +1,43 @@
+return `
+<!DOCTYPE html> 
+<html lang="en"> 
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Portfolio Demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+
+<i class="bi bi-door-open"></i>
+<i class="bi bi-eyeglasses"></i>
+<i class="bi bi-book"></i>
+<i class="bi bi-clock-history"></i>
+
+
+<body>
+<header>
+  <div class="container flex-row justify-space-between align-center py-3">
+    <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+    <nav class="flex-row">
+      <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
+        header.github
+      }">GitHub</a>
+    </nav>
+  </div>
+</header>
+<main class="container my-5">
+${generateAbout(about)}
+</main>
+<footer class="container text-center py-3">
+  <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+</footer>
+</body>
+</html>
+`;
